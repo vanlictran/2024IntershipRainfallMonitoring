790645ed3b26f7e2d4a43142fb0506bb5b6dded7
Tsukoyachi <Axel.delille31@gmail.com>
2024-08-07 13:08:19 +0700

    feat: Addition of other metric for the rain gauge and visualization on grafana

--- server/mqtt_prometheus_exporter/mqtt_exporter_prometheus.py
+++ server/mqtt_prometheus_exporter/mqtt_exporter_prometheus.py
@@ -5,9 +5,15 @@ from prometheus_client import start_http_server, Gauge
 import time
 
 rain_data = {}
+humidity_data = {}
+temperature_data = {}
+voltage_data = {}
 water_level_data = {}
 
 rain_gauge = Gauge('rain_amount', 'Amount of rain measured', ['devEUI'])
+humidity_gauge = Gauge('humidity_percentage', 'Percentage of humidity measured', ['devEUI'])
+temperature_gauge = Gauge('temperature_level', 'Temperature measured', ['devEUI'])
+voltage_gauge = Gauge('voltage_level', 'Voltage used by the rain gauge', ['devEUI'])
 water_level_gauge = Gauge('water_level', 'Water level measured', ['devEUI'])
 
 # Callback when a message is received
@@ -29,12 +35,33 @@ def on_message(client, userdata, message):
         print(f"Missing 'object' : {payload_str}")
         return
     
-    if 'data' in payload['object'] and payload['object']['data'].get('rain') is not None :
-        # Update the global dictionary
-        rain_value = payload['object']['data'].get('rain')
-        rain_data[deveui] = rain_value
-        rain_gauge.labels(devEUI=deveui).set(rain_value)
-        print(f"Updated rain data for devEUI '{deveui}': {rain_value}")
+    if 'data' in payload['object'] :
+        data = payload['object']['data']
+        
+        rain_value = data.get('rain')
+        temperature_value = data.get('temperature')
+        humidity_value = data.get('humidity')
+        voltage_value = data.get('voltage')
+        
+        if rain_value is not None :
+            rain_data[deveui] = rain_value
+            rain_gauge.labels(devEUI=deveui).set(rain_value)
+            print(f"Updated rain data for devEUI '{deveui}': {rain_value}")
+        
+        if temperature_value is not None :
+            temperature_data[deveui] = temperature_data
+            temperature_gauge.labels(devEUI=deveui).set(temperature_value)
+            print(f"Updated temperature data for devEUI '{deveui}': {temperature_value}")
+        
+        if humidity_value is not None :
+            humidity_data[deveui] = humidity_value
+            humidity_gauge.labels(devEUI=deveui).set(humidity_value)
+            print(f"Updated humidity data for devEUI '{deveui}': {humidity_value}")
+        
+        if voltage_value is not None :
+            voltage_data[deveui] = voltage_value
+            voltage_gauge.labels(devEUI=deveui).set(voltage_value)
+            print(f"Updated voltage data for devEUI '{deveui}': {voltage_value}")
         
     elif payload['object'].get('waterLevel') is not None :
         water_level_value = payload['object'].get('waterLevel')
